a082e18769be065bbcf915581f15f1038bf3d87e
Alis-Hdz <isaac.aldo@gmail.com>
2025-08-14 21:06:58 -0600

    Multiples ejercicios

--- 02.Clases/semana1/Ejercicio5MIO.js
+++ 02.Clases/semana1/Ejercicio5MIO.js
@@ -16,20 +16,15 @@ async function main() {
   if (temperatura < 10) {
     console.log("Hace frío");
   }
-  else if (temperatura >=10 && temperatura<20)
+  else if (temperatura >=10 && temperatura<20){
     console.log("Esta templado");
-
-
-
-
-
-
-
-
-
-  // TODO: Implementar la lógica para temperaturas entre 10 y 20 grados
-  // TODO: Implementar la lógica para temperaturas entre 20 y 30 grados
-  // TODO: Implementar la lógica para temperaturas superiores a 30 grados
+  }
+  else if (temperatura >=20 && temperatura <30){
+    console.log("Hace calor");
+  }
+  else if (temperatura >30){
+    console.log("Hace mucho calor");
+  }
 
 }
 
