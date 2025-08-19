6f6f20173b18c64211b92d15722639afc049a1c7
Alis-Hdz <isaac.aldo@gmail.com>
2025-08-18 20:20:18 -0600

    semana 2 y primer ejercicio con el packagejson

---
+++ 02.Clases/semana2/Ejercicio1MINE.js
@@ -0,0 +1,32 @@
+const { ask } = require('../helpers/input.js');
+
+async function saludar (nombre){
+    console.log (`Hola estimado(a): ${nombre}, espero que tengas un buen día ...`)
+}
+
+
+
+async function main() {
+
+    const personas = [ "Aldo", "Isaac", "Adriana", "Ely", "Adriana Grandota"]
+
+    console.log ("-----------------------Ascendente-----------------------------")
+    
+    for (let i = 0; i < personas.length; i++){
+
+        saludar(personas[i]);
+
+    }
+
+    console.log ("-----------------------Descendente-----------------------------")
+
+    for (let j = personas.length - 1; j >= 0; j--){
+
+        saludar(personas[j]);
+
+    }
+
+}
+
+
+main();
